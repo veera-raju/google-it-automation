5fb7791d2bab795db13dfd73a24add20aa20c399
veera <77234681+veera-raju@users.noreply.github.com>
2021-11-09 22:14:05 +0800

    Update network.py

--- c2_python-operating-system/1_getting-your-python-on/graded-assessment/scripts/network.py
+++ c2_python-operating-system/1_getting-your-python-on/graded-assessment/scripts/network.py
@@ -6,7 +6,15 @@ import socket
 def check_localhost():
     localhost = socket.gethostbyname('localhost')
     return localhost == '127.0.0.1'
+        if check_localhost == localhost:
+           return true
+        else:
+           return false 
 
 def check_connectivity():
     request = requests.get("http://www.google.com")
+        if check_connectivity == 200:
+            return true 
+        else:
+            return false
     return 200
